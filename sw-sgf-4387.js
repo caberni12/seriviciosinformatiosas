7b297fc8314ae2a46c2940fcd26e41053e389877
caberni12 <alejandrosilva1891@gmail.com>
2026-08-20 01:07:19 -0400

    Add files via upload

--- sw-sgf-4387.js
+++ sw-sgf-4387.js
@@ -1,9 +1,9 @@
-const SGF_CACHE='efleet-static-4.3.91-geo-modal-size';
+const SGF_CACHE='efleet-static-4.3.92-geo-plan-acumulativo-check';
 const PRECACHE=[
   './','./index.html','./main.html','./logo.svg','./logo-e-fleet.png','./efleet-mark-compact.png','./favicon-efleet.png','./tema.js','./estilos.css','./responsive.css','./interfaz-moderna.css',
   './menu-principal.css','./sgf-final-4.3.44.css','./efleet-palette.4386.css','./efleet-lightblue.4388.css','./login-r8245.css','./sgf-performance.4387.js','./sgf-module.4387.js','./sgf-shell.4387.js',
   './panel-principal.html','./rutas.html','./operaciones.html','./checkin-vehicular.html','./vehiculos.html','./conductores.html',
-  './geo-local.html','./geo-local-admin.html','./mapa-geo-local.html','./kpi-geo-locales.html','./malla-turnos.html','./geo-local.4391.css','./geo-local-common.4390.js','./mapa.js'
+  './geo-local.html','./geo-local-admin.html','./mapa-geo-local.html','./kpi-geo-locales.html','./malla-turnos.html','./geo-local.4392.css','./geo-local-common.4392.js','./mapa.js'
 ];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(SGF_CACHE).then(async cache=>{
